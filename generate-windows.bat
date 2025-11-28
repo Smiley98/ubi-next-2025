ed6914fd8a317ef82d2d86bd1d3f6656246b75d0
Smiley98 <smileysofficialcinema@gmail.com>
2025-11-28 12:06:09 -0500

    Moved open VS Solution to windows project gen script

--- generate-windows.bat
+++ generate-windows.bat
@@ -1,1 +1,2 @@
 cmake -B build/win64 -DCMAKE_TOOLCHAIN_FILE=./TC-windows.cmake
+start "" "%~dp0build\win64\UbiTorContestAPI.sln"
--- open-sln.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start "" "%~dp0build\win64\UbiTorContestAPI.sln"
