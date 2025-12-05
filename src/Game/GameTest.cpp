8454b39f2528136c754197a502da0be8f744b059
Smiley98 <smileysofficialcinema@gmail.com>
2025-12-05 15:10:45 -0500

    Rendered plane

--- src/Game/GameTest.cpp
+++ src/Game/GameTest.cpp
@@ -4,24 +4,23 @@
 
 #include <iostream>
 #include <vector>
+#include <cassert>
 #include "raymath.h"
 #include "../ContestAPI/app.h"
+
+enum MeshType
+{
+	MESH_TRIANGLE,
+	MESH_PLANE,
+	MESH_TYPE_COUNT
+};
 
 struct Mesh
 {
 	size_t face_count = 0;
 	std::vector<Vector3> positions;	// size is face_count * 3
 	std::vector<Vector3> normals;	// size is face_count * 3
-	std::vector<uint16_t> indices;
 };
-
-void UnloadMesh(Mesh* mesh)
-{
-	mesh->positions.resize(0);
-	mesh->normals.resize(0);
-	mesh->indices.resize(0);
-	mesh->face_count = 0;
-}
 
 struct UniformData
 {
@@ -32,7 +31,108 @@ struct UniformData
 	Vector3 light_direction;
 };
 
-void DrawMesh(Mesh mesh, const UniformData& data, bool wireframe = false)
+void TriangulateMesh(Mesh* mesh, std::vector<uint16_t> indices);
+void UnloadMesh(Mesh* mesh);
+void DrawMesh(Mesh mesh, const UniformData& data, bool wireframe = false);
+
+static float tt = 0.0f;
+static Mesh meshes[MESH_TYPE_COUNT];
+
+void Init()
+{
+	{
+		Mesh& m = meshes[MESH_TRIANGLE];
+		m.face_count = 1;
+
+		m.positions.resize(m.face_count * 3);
+		m.positions[0] = { 0.5f, -0.5f, 0.0f };
+		m.positions[1] = { 0.0f,  0.5f, 0.0f };
+		m.positions[2] = { -0.5f, -0.5f, 0.0f };
+
+		m.normals.resize(m.face_count * 3);
+		for (Vector3& n : m.normals)
+			n = Vector3UnitZ;
+	}
+
+	{
+		Mesh& m = meshes[MESH_PLANE];
+		m.positions.resize(4);
+		m.positions[0] = { 0.5f, -0.5f, 0.0f };		// bottom-right
+		m.positions[1] = { 0.5f,  0.5f, 0.0f };		// top-right
+		m.positions[2] = { -0.5f,  0.5f, 0.0f };	// top-left
+		m.positions[3] = { -0.5f, -0.5f, 0.0f };	// bottom-left
+
+		m.normals.resize(4);
+		for (Vector3& n : m.normals)
+			n = Vector3UnitZ;
+
+		std::vector<uint16_t> indices
+		{
+			0, 1, 3,
+			1, 2, 3
+		};
+
+		TriangulateMesh(&m, indices);
+	}
+
+	// Initialization sanity-check
+	for (int i = 0; i < MESH_TYPE_COUNT; i++)
+		assert(meshes[i].face_count > 0);
+}
+
+void Update(const float deltaTime)
+{
+	const float dt = deltaTime / 1000.0f;
+	tt += dt;
+}
+
+void Render()
+{
+	Matrix world = MatrixRotateZ(100.0f * tt * DEG2RAD) * MatrixRotateY(50.0f * tt * DEG2RAD) * MatrixTranslate(0.0f, 0.0f, 5.0f + sinf(tt) * 4.5f);
+	Matrix view = MatrixLookAt({ 0.0f, 0.0f, 10.0f }, Vector3Zeros, Vector3UnitY);
+	Matrix proj = MatrixPerspective(90.0f * DEG2RAD, APP_VIRTUAL_WIDTH / (float)APP_VIRTUAL_HEIGHT, 0.1f, 100.0f);
+
+	UniformData data;
+	data.world = world;
+	data.mvp = world * view * proj;
+
+	const CController& cont = CSimpleControllers::GetInstance().GetController();
+
+	static int mesh = MESH_TRIANGLE;
+	if (cont.CheckButton(App::BTN_DPAD_DOWN)) // Emulated as KEY_K
+		++mesh %= MESH_TYPE_COUNT;
+	
+	static bool wireframe = false;
+	if (cont.CheckButton(App::BTN_DPAD_UP)) // Emulated as KEY_I
+		wireframe = !wireframe;
+
+	DrawMesh(meshes[mesh], data, wireframe);
+}
+
+void Shutdown()
+{
+	for (int i = 0; i < MESH_TYPE_COUNT; i++)
+		UnloadMesh(&meshes[i]);
+}
+
+void TriangulateMesh(Mesh* mesh, std::vector<uint16_t> indices)
+{
+	std::vector<Vector3> positions_src = mesh->positions;
+	std::vector<Vector3> normals_src = mesh->normals;
+
+	mesh->positions.resize(indices.size());
+	mesh->normals.resize(indices.size());
+	mesh->face_count = indices.size() / 3;
+
+	for (size_t i = 0; i < mesh->face_count * 3; i++)
+	{
+		uint16_t index = indices[i];
+		mesh->positions[i] = positions_src[index];
+		mesh->normals[i] = normals_src[index];
+	}
+}
+
+void DrawMesh(Mesh mesh, const UniformData& data, bool wireframe)
 {
 	Matrix normal_matrix = MatrixNormal(data.world);
 	for (size_t f = 0; f < mesh.face_count; f++)
@@ -78,81 +178,17 @@ void DrawMesh(Mesh mesh, const UniformData& data, bool wireframe = false)
 		Vector3 n2 = world_normals[2];
 		Vector3 n = n0;	// <-- all 3 normals are identical per-face.
 
+		// Color based on normals remapped from [-1, 1] --> [0, 1]
 		Vector3 c = n;
+		c *= 0.5f;
+		c += Vector3Ones * 0.5f;
 		App::DrawTriangle(v0.x, v0.y, v1.x, v1.y, v2.x, v2.y, c.x, c.y, c.z, wireframe);
 	}
 }
 
-enum MeshType
+void UnloadMesh(Mesh* mesh)
 {
-	MESH_TRIANGLE,
-	MESH_PLANE,
-	MESH_TYPE_COUNT
-};
-
-static float tt = 0.0f;
-static Mesh meshes[MESH_TYPE_COUNT];
-
-void Init()
-{
-	{
-		Mesh& m = meshes[MESH_TRIANGLE];
-		m.face_count = 1;
-
-		m.positions.resize(m.face_count * 3);
-		m.positions[0] = { 0.5f, -0.5f, 0.0f };
-		m.positions[1] = { 0.0f,  0.5f, 0.0f };
-		m.positions[2] = { -0.5f, -0.5f, 0.0f };
-
-		m.normals.resize(m.face_count * 3);
-		for (Vector3& n : m.normals)
-			n = Vector3UnitZ;
-	}
-
-	{
-		std::vector<Vector3> plane_positions
-		{
-			{  0.5f, -0.5f, 0.0f },	// bottom-right
-			{  0.5f,  0.5f, 0.0f },	// top-right
-			{ -0.5f,  0.5f, 0.0f },	// top-left
-			{ -0.5f, -0.5f, 0.0f },	// bottom-left
-		};
-
-		std::vector<uint16_t> plane_indices
-		{
-			0, 1, 3,
-			1, 2, 3
-		};
-	}
-	
-	// TODO -- figure out how to use index buffer for meshes with more than 3 vertices xD xD xD
-}
-
-void Update(const float deltaTime)
-{
-	const float dt = deltaTime / 1000.0f;
-	tt += dt;
-}
-
-void Render()
-{
-	Matrix world = MatrixRotateZ(100.0f * tt * DEG2RAD) * MatrixTranslate(0.0f, 0.0f, 5.0f + sinf(tt) * 4.5f);
-	Matrix view = MatrixLookAt({ 0.0f, 0.0f, 10.0f }, Vector3Zeros, Vector3UnitY);
-	Matrix proj = MatrixPerspective(90.0f * DEG2RAD, APP_VIRTUAL_WIDTH / (float)APP_VIRTUAL_HEIGHT, 0.1f, 100.0f);
-
-	UniformData data;
-	data.world = world;
-	data.mvp = world * view * proj;
-
-	static int mesh = MESH_TRIANGLE;
-	if (App::IsKeyPressed(App::KEY_TAB))
-		++mesh %= MESH_TYPE_COUNT;
-
-	DrawMesh(meshes[mesh], data);
-}
-
-void Shutdown()
-{
-	for (int i = 0; i < MESH_TYPE_COUNT; i++)
-		UnloadMesh(&meshes[i]);
+	mesh->positions.resize(0);
+	mesh->normals.resize(0);
+	mesh->face_count = 0;
 }
